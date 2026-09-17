155f73fed5b24f089b2ec786871b029ef18c64bb
pos <pgousdal@users.noreply.github.com>
2026-09-18 00:21:08 +0200

    Wire M1.19 test to extended EA interface

--- sim/tb_m1_19_pc_relative_ea.sv
+++ sim/tb_m1_19_pc_relative_ea.sv
@@ -6,6 +6,7 @@ module tb_m1_19_pc_relative_ea;
     logic [31:0] pc_base;
     logic [15:0] extension_hi;
     logic [15:0] extension_lo;
+    logic [31:0] index_register;
     logic [31:0] effective_address;
     logic [2:0] extension_bytes;
 
@@ -15,6 +16,7 @@ module tb_m1_19_pc_relative_ea;
         .pc_base(pc_base),
         .extension_hi(extension_hi),
         .extension_lo(extension_lo),
+        .index_register(index_register),
         .effective_address(effective_address),
         .extension_bytes(extension_bytes)
     );
@@ -40,6 +42,7 @@ module tb_m1_19_pc_relative_ea;
     endtask
 
     initial begin
+        index_register = 32'd0;
         check_pc_relative(32'h00000102, 16'h001e, 32'h00000120);
         check_pc_relative(32'h00000120, 16'hffe0, 32'h00000100);
         check_pc_relative(32'h00ffff00, 16'h0100, 32'h01000000);
